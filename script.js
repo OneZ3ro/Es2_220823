d4abe1bb2554753d05155e4995f357560f696143
Angelo Moreno <emilioangelo665@gmail.com>
2023-08-22 16:24:26 +0200

    stopwatch done

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-window.addEventListener("DOMContentLoaded", () => {
-  manageTextInput();
-});
-
 const btn1 = document.querySelector("#btn-1");
 const btn2 = document.querySelector("#btn-2");
 const input = document.querySelector("input");
 const form = document.querySelector("form");
 const p = document.querySelector("#preText");
+const spanMin = document.querySelector("#minuti");
+const spanSec = document.querySelector("#secondi");
+const spanMill = document.querySelector("#mill");
+let mins = 0;
+let secs = 0;
+let mills = 0;
+let interval = null;
 
 const takeInput = () => {
   let text = input.value;
@@ -27,5 +30,48 @@ const manageTextInput = () => {
   }
 };
 
+const counter = () => {
+  mills++;
+  if (mills < 9) {
+    spanMill.innerText = "0" + mills;
+  }
+  if (mills > 9) {
+    spanMill.innerText = mills;
+  }
+  if (mills > 99) {
+    secs++;
+    if (secs <= 9) {
+      spanSec.innerText = "0" + secs;
+    } else if (secs > 9) {
+      spanSec.innerText = secs;
+    }
+    mills = 0;
+    spanMill.innerText = "0" + 0;
+  }
+  if (secs > 59) {
+    mins++;
+    if (mins <= 9) {
+      spanMin.innerText = "0" + mins;
+    } else if (mins > 9) {
+      spanMin.innerText = mins;
+    }
+    secs = 0;
+    spanSec.innerText = "0" + 0;
+    mills = 0;
+    spanMill.innerText = "0" + 0;
+  }
+  if (mins > 9) {
+    spanMin.innerText = mins;
+  }
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  manageTextInput();
+
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(counter, 10);
+});
 btn1.onclick = takeInput;
 btn2.onclick = removeInput;
